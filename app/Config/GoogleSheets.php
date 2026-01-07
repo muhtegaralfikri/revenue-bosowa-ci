0a6171f900d4391e1baa06e940226129f618d1b4
muhtegaralfikri <105841110722@student.unismuh.ac.id>
2026-01-07 22:25:36 +0800

    Handle absolute Google Sheets credentials path

--- app/Config/GoogleSheets.php
+++ app/Config/GoogleSheets.php
@@ -16,17 +16,28 @@ class GoogleSheets extends BaseConfig
         parent::__construct();
         
         $this->enabled = env('google.sheets.enabled', false) === 'true' || env('google.sheets.enabled', false) === true;
-        $credentialsPath = env('google.sheets.credentials_file', 'google-credentials.json');
-        
-        // Convert relative path to absolute path
-        if (!is_file($credentialsPath) && strpos($credentialsPath, ':') === false) {
-            // It's a relative path, prepend with WRITEPATH
-            $this->credentialsFile = WRITEPATH . ltrim($credentialsPath, '/\\');
-        } else {
+        $credentialsPath = trim((string) env('google.sheets.credentials_file', 'google-credentials.json'), " \t\n\r\0\x0B\"'");
+
+        // Respect absolute paths (Unix or Windows) even if the file is missing;
+        // otherwise, treat as relative to the writable directory.
+        if ($this->isAbsolutePath($credentialsPath)) {
             $this->credentialsFile = $credentialsPath;
+        } else {
+            $this->credentialsFile = rtrim(WRITEPATH, '/\\') . DIRECTORY_SEPARATOR . ltrim($credentialsPath, '/\\');
         }
         
         $this->spreadsheetId = env('google.spreadsheet.id', '');
         $this->syncInterval = (int) env('google.sheets.sync_interval', 60);
+    }
+
+    private function isAbsolutePath(string $path): bool
+    {
+        if ($path === '') {
+            return false;
+        }
+
+        return $path[0] === '/' // Unix-style absolute path
+            || $path[0] === '\\' // UNC path
+            || preg_match('#^[A-Za-z]:[\\\\/]#', $path) === 1; // Windows drive letter
     }
 }
